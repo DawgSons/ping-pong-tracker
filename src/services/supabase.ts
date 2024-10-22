f414d97d6b1fcc412b2fd04f0ef066138a277b49
Fabian Seiffert <GOSCHEN@users.noreply.github.com>
2024-10-22 11:10:44 +0200

    framework tests

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "../types/supabase"
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string;
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase environment variables');
--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,38 @@
-import path from 'path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import {VitePWA} from 'vite-plugin-pwa';
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'icons/*.png'],
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "robots.txt", "icons/*.png"],
       manifest: {
-        name: 'PingPong Finder',
-        short_name: 'PPFinder',
-        description: 'Find ping pong tables near you',
-        theme_color: '#ffffff',
+        name: "PingPong Finder",
+        short_name: "PPFinder",
+        description: "Find ping pong tables near you",
+        theme_color: "#ffffff",
         icons: [
           {
-            src: '/icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "/icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: '/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })],
-    resolve:{
-      alias:{
-        "@": path.resolve(__dirname, "./src"),
+            src: "/icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
-    }
-})
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
